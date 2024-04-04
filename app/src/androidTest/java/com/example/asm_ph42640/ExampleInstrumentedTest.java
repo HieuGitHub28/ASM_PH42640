b491a6f679f0941991830eff5caa7c097147b3a2
HieuGitHub28 <phamhieu28042004@gmail.com>
2024-04-04 13:26:07 +0700

    commnit

--- app/src/androidTest/java/com/example/asm_ph42640/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/asm_ph42640/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.asm_ph42693", appContext.getPackageName());
+        assertEquals("com.example.asm_ph42640", appContext.getPackageName());
     }
 }
